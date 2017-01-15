77fa801a890fbcdf3d665a7d0db7e1264cd76215
restrellado <ryan.estrellado@gmail.com>
2017-01-14 21:17:56 -0800

    Mediations held vs. agreements reached.

--- clean_dr.R
+++ clean_dr.R
@@ -1,6 +1,7 @@
 # Download and read 13-14 dispute resolution data
 setwd("~/Documents/github/dispute_resolution_data")
-url.name <- "http://www2.ed.gov/programs/osepidea/618-data/state-level-data-files/part-b-data/dispute-resolution/bdispres2012-13.csv"
-download.file(url.name, destfile = "dr_12_13.csv", method = "curl")
-
-dr <- read.csv("dr_12_13.csv", stringsAsFactors = FALSE, skip = 4, nrows = 61)
+if (file.exists("bdispres2013-14.csv") == FALSE) {
+  url.name <- "http://www2.ed.gov/programs/osepidea/618-data/state-level-data-files/part-b-data/dispute-resolution/bdispres2012-13.csv"
+  download.file(url.name, destfile = "bdispres2013-14.csv", method = "curl")
+  dr <- read.csv("bdispres2013-14.csv", stringsAsFactors = FALSE, skip = 4, nrows = 61) 
+}
